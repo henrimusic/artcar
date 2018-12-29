469f6c98f23da3a59c0b386e1d1fff1fc699a194
henrimusic <henrimus.96@hotmail.com>
2018-12-29 17:44:09 -0200

    rotas alter

--- app/rotas/rotasForm.js
+++ app/rotas/rotasForm.js
@@ -1,18 +1,17 @@
 module.exports = (app) => { 
+	const fs = require('fs');
 	
-	// const fs = require('fs');
-	
-	// // Configuração do Multer
-	// const multer = require('multer');
-	// const storage = multer.diskStorage({
-	// 	destination: function(req, file, callback){
-	// 		callback(null, './public/style/img9/');
-	// 	},
-	// 	filename: function(req, file, callback){
-	// 		callback(null, file.originalname);
-	// 	}
-	// });
-	// const upload = multer({storage: storage});
+	// Configuração do Multer
+	const multer = require('multer');
+	const storage = multer.diskStorage({
+		destination: function(req, file, callback){
+			callback(null, './public/style/img9/');
+		},
+		filename: function(req, file, callback){
+			callback(null, file.originalname);
+		}
+	});
+	const upload = multer({storage: storage});
 
 	// Página de Formulário do Veúclo
 	app.get('/wb-admin/form',  function(req, res){
@@ -20,8 +19,7 @@ module.exports = (app) => {
 	});
 
 	// Gravar no banco
-	app.post('/wb-admin/form', function(req, res){
-		// upload.single('imagem'),
+	app.post('/wb-admin/form', upload.single('imagem'), function(req, res){
 		var imagem = [];
 		var connection = app.infra.connectionFactory();
 		var veiculoDAO = new app.infra.VeiculoDAO(connection);
@@ -60,14 +58,14 @@ module.exports = (app) => {
 			// cria um novo veiculo no banco
 			veiculo.idVeiculo = Math.floor(Math.random() * (1000 - 0)) + 1;
 
-			// imagem.push({
-			// 	path: req.file.path,
-			// 	idVeiculo: veiculo.idVeiculo
-			// });	
+			imagem.push({
+				path: req.file.path,
+				idVeiculo: veiculo.idVeiculo
+			});	
 			veiculoDAO.salva(veiculo, function(erro, resultado){});
-			// veiculoDAO.salvaImagem(imagem, function(erro, resultado){
-			// 	res.redirect('/wb-admin/lista');
-			// });
+			veiculoDAO.salvaImagem(imagem, function(erro, resultado){
+				res.redirect('/wb-admin/lista');
+			});
 			connection.end();
 		}
 	});
