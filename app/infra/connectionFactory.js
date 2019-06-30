a9dfa5425d6c0572a06c4bf0714bda93a7ed638f
Henrique Augusto <henrimus.96@hotmail.com>
2019-06-30 13:18:44 -0300

    Update connectionFactory.js

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 function createDBConnection() {
 	return mysql.createConnection({
-		host : 'us-cdbr-iron-east-03.cleardb.net',
-		user : 'b8423443422de2',
-		password :'75b9f065',
-		database : 'heroku_ec2292d2a11de58'
+		host : 'artcar.mysql.uhserver.com',
+		user : 'artcar',
+		password :'474109Has@',
+		database : 'artcar'
 	});
 }
 
